2eaff48bcf9f17b5329c2da1356d0b725a7cd15d
Ali Aryani <lenesis@proton.me>
2025-08-06 09:37:17 +0330

    add error handling

--- client.py
+++ client.py
@@ -5,9 +5,12 @@ import socket
 client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 8080
-
-client.connect ((host, port)) # connect to our client
-msg = client.recv (1024)
-client.close()
-
-print (msg.decode('ascii'))
+try:
+    client.connect ((host, port)) # connect to our client
+    msg = client.recv (1024)
+    client.close()
+    print (msg.decode('ascii'))
+except ConnectionRefusedError:
+    print ("Server did not accept our request for connection!")
+    exit (1)
+print ("If you see this, it means the except block was not executed.")
