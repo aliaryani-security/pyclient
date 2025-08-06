7cda8e0934bc472d42732088861f19ee96922371
Ali Aryani <lenesis@proton.me>
2025-08-06 09:29:18 +0330

    connect and disconnect

--- client.py
+++ client.py
@@ -3,3 +3,11 @@
 import socket
 
 client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 8080
+
+client.connect ((host, port)) # connect to our client
+msg = client.recv (1024)
+client.close()
+
+print (msg.decode('ascii'))
